174c25cd38db2093317c6e846e12f627484f6199
AnanyaFPT <AnanyaDS@fpt.com>
2024-08-18 11:26:18 -0600

    added latitude longitude filtering

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -37,6 +37,10 @@ def go(args):
     logger.info('converting last_review column to date type')
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    # ensure latitute and longitude are correct
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     # save cleaned data to file 
     logger.info('saving cleaned data to clean_sample.csv')
     df.to_csv("clean_sample.csv", index=False)
